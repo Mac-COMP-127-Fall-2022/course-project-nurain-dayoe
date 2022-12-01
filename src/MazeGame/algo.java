53a79d70e6af38aed8dfaba812204b182f5f1ee9
NurainJ <109047209+NurainJ@users.noreply.github.com>
2022-12-01 12:56:06 -0600

    Converted Generation Algorithm

--- src/MazeGame/algo.java
+++ src/MazeGame/algo.java
@@ -1,24 +1,34 @@
 package MazeGame;
 
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsGroup;
+import edu.macalester.graphics.Image;
 import edu.macalester.graphics.Point;
 import java.util.*;
-import java.util.stream.Collector;
-import java.util.stream.Stream;
 
 import MazeGame.MazeGame.Side;
 
 public class algo {
-    private int[][] Matrix = new int[100][100], solutionMatrix = new int[100][100];
+    public int[][] matrix = new int[100][100];//, solutionMatrix = new int[100][100];
     private ArrayList<Point> referencePoints = new ArrayList<Point>();
+    private ArrayList<Side> sides = new ArrayList<>(List.of(Side.RIGHT, Side.LEFT, Side.BOTTOM, Side.TOP));
     private boolean firstIteration = true;
     private Random rand = new Random();
+    public int counter = 0;
 
     public algo(){
-        for (int x = 1; x < 50; x += 2) {
+        createPath(new Point(0,0), new Point (99, 99));
+        createPath(new Point (89, 12), new Point(2, 95));
+        
+    }
+
+    private void createPath(Point initial, Point last) {
+        firstIteration = true;
+        for (int x = 1; x < 25; x += 1) {
             Point point1, point2;
             if (firstIteration) {
-                point1 = new Point(7,0);
-                point2 = new Point(98, 100);
+                point1 = initial;
+                point2 = last;
             } else {
                 if (!referencePoints.isEmpty()) {
                     int randomIndex = rand.nextInt(referencePoints.size());
@@ -30,81 +40,101 @@ public class algo {
                 point2 = new Point(rand.nextInt(100), rand.nextInt(100));
             }
 
-            while (point1.getX() != point2.getX() || point1.getY() != point2.getY()) {
+            while ((point1.getX() != point2.getX() || point1.getY() != point2.getY())) {
+                counter++;
                 point1 = movePointer(point1, point2, x);
-                if (rand.nextFloat() > 0.1) {
+                if (rand.nextFloat() > 0.01) {
                     referencePoints.add(point1);
                 }
             }
         }
-        
     }
 
-    private HashMap<Side, Boolean> generateWeights(Point p1, Point p2) {
-        HashMap<Side, Boolean> weights = new HashMap<>(Map.of(Side.TOP, false, Side.BOTTOM, false, Side.RIGHT, false, Side.LEFT, false));
+    private ArrayList<Side> generateWeights(Point p1, Point p2) {
+        ArrayList<Side> weights = new ArrayList<MazeGame.Side>(4);
         if (p2.getX() > p1.getX()) {
-            weights.replace(Side.RIGHT, true);
+            weights.add(Side.RIGHT);
         } else if (p2.getX() < p1.getX()){
-            weights.replace(Side.LEFT, true);
+            weights.add(Side.LEFT);
         }
 
-        if (p2.getY() > p2.getY()) {
-            weights.replace(Side.TOP, true);
-        } else if (p2.getY() < p2.getY()){
-            weights.replace(Side.BOTTOM, true);
+        if (p2.getY() > p1.getY()) {
+            weights.add(Side.TOP);
+        } else if (p2.getY() < p1.getY()){
+            weights.add(Side.BOTTOM);
         }
 
         return weights;
     }
 
     private Point movePointer(Point point1, Point point2, int layerLabel) {
-        HashMap<Side, Boolean> weights = generateWeights(point1, point2);
+        ArrayList<Side> weights = generateWeights(point1, point2);
         int[] currentPoint = {(int) point1.getX(), (int) point1.getY()};
         if (firstIteration) {
-            solutionMatrix[currentPoint[0]][currentPoint[1]] = 1;
+            //solutionMatrix[currentPoint[0]][currentPoint[1]] = 1;
             firstIteration = false;
         }
-        boolean repeat = true;
-        do {
-            switch(rand.nextInt(4)) {
-                case 0:
-                    if (weights.get(Side.TOP)) {
-                        currentPoint[1]++;
-                        repeat = false;
-                    }
+        if (matrix[currentPoint[0]][currentPoint[1]] == 0) {
+            matrix[currentPoint[0]][currentPoint[1]] = 1;
+        }
+        Side moveDirection = weights.get(rand.nextInt(weights.size()));
+
+        if (rand.nextFloat() < 0.2) { //allow a 20% chance that the direction is randomly picked, allowing for the option of a "wrong" direction
+            moveDirection = sides.get(rand.nextInt(4));
+        }
+
+        switch (moveDirection) {
+            case RIGHT:
+                if (currentPoint[0] < 99) {
+                    currentPoint[0]++;
+                }
                     break;
-                case 1:
-                    if (weights.get(Side.RIGHT)) {
-                        currentPoint[0]++;
-                        repeat = false;
-                    }
-                    break;
-                case 2:
-                    if (weights.get(Side.BOTTOM)) {
-                        currentPoint[1]--;
-                        repeat = false;
-                    }
-                    break;
-                case 3:
-                    if (weights.get(Side.LEFT)) {
-                        currentPoint[0]--;
-                        repeat = false;
-                    }
-                    break;
-                default: 
-                    break;  
+            case TOP:
+                if (currentPoint[1] < 99) {
+                    currentPoint[1]++;
+                }
+                break;
+            case LEFT:
+                if (currentPoint[0] > 0) {
+                    currentPoint[0]--;
+                }
+                break;
+            case BOTTOM:
+            if (currentPoint[1] > 0) {
+                currentPoint[1]--;
             }
-        } while (repeat);
+                break;
+            default:
+                break;
+        }
 
         return new Point(currentPoint[0], currentPoint[1]);
     }
     
     public static void main(String[] args) {
         algo temp = new algo();
-        for (int[] row : temp.Matrix) {
-            for(int cell : row){
-                System.out.print(cell);
+        System.out.println("Running");
+        CanvasWindow canvas = new CanvasWindow("Test", 1000,1000);
+        GraphicsGroup group = new GraphicsGroup();
+        for (int x = 0; x < 100; x++) {
+            for(int y = 0; y < 100; y++){
+                if (temp.matrix[x][y] == 0) {
+                    Image image = new Image("grass.jpg");
+                    group.add(image);
+                    image.setPosition(x * 40, y * 40);
+                } else if (temp.matrix[x][y] == 1) {
+                    Image image = new Image("respack/12.jpg");
+                    group.add(image);
+                    image.setPosition(x * 40, y * 40);
+                } else {
+                    Image image = new Image("respack/11.jpg");
+                    group.add(image);
+                    image.setPosition(x * 40, y * 40);
+                }
             }
+            group.setScale(0.2);
+            canvas.add(group);
+            group.setCenter(canvas.getCenter());
             System.out.println("");
         }
     }
